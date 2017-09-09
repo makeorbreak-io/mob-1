379fe020217882e4f420f9732974cf1d7d80495c
rmntavares <ricardo@ricardomnt.com>
2017-09-09 16:33:33 +0100

    QR code function

--- Portos/src/app/app.module.ts
+++ Portos/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { HTTP } from '@ionic-native/http';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { SendMoneyPage, FirstSC , CustomPopup } from '../pages/pages';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -40,6 +40,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   providers: [
     StatusBar,
+    BarcodeScanner,
+    HTTP,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ],
--- Portos/src/pages/custom-popup/custom-popup.ts
+++ Portos/src/pages/custom-popup/custom-popup.ts
@@ -1,5 +1,8 @@
 import { Component, Renderer } from '@angular/core';
 import {  ViewController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { HTTP } from '@ionic-native/http';
+
 
 @Component({
   selector: 'custom-popup',
@@ -10,12 +13,38 @@ export class CustomPopup {
   text: string;
 
   constructor(public renderer: Renderer, 
-              public viewCtrl: ViewController) {
+              public viewCtrl: ViewController,
+              public barcodeScanner: BarcodeScanner,
+              private http: HTTP) {
     this.renderer.setElementClass(viewCtrl.pageRef().nativeElement, 'custom-popup', true);
 
   }
 
   popOut(){
     this.viewCtrl.dismiss();
+  }
+
+  scanQR(){
+    this.barcodeScanner.scan().then((barcodeData) => {
+      // Success! Barcode data is here
+     }, (err) => {
+         // An error occurred
+     });
+  }
+
+  getQR(code){
+    var url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+code;
+
+    this.http.get(url, {}, {})
+    .then(data => {
+    var qr = data.data; // data received by server
+  
+    })
+    .catch(error => {
+  
+      var qrerror = error.error; // error message as string
+  
+    });
+    
   }
 }
